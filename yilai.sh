6576e6de1e599ac39b3b0362a7ef94227321b9d8
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2021-07-16 23:25:40 +0800

    Add files via upload

---
+++ yilai.sh
@@ -0,0 +1,11 @@
+
+cp -r /ql/repo/LJMX996_jd_aaron/utils /ql/scripts
+
+npm install axios date-fns
+
+
+cd scripts && npm i -S png-js
+
+apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
+
+cd /ql/scripts && npm install canvas --build-from-source
