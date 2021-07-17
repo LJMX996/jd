e36e9d23c271396e2436a365df398ed66e471daf
刘庆 <lq510885@qq.com>
2021-07-17 22:56:16 +0800

    Update

--- yilai.sh
+++ yilai.sh
@@ -1,1 +1,7 @@
 cp -rf /ql/repo/LJMX996_jd_aaron/utils /ql/scripts/
+
+npm install axios date-fns
+
+cd /ql/scripts && npm i -S png-js
+
+apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd /ql/scripts && npm install canvas --build-from-source
