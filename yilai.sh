30b68920cc1c0dccfba201228857455592da87fe
刘庆 <lq510885@qq.com>
2021-07-18 08:33:05 +0800

    Update

--- yilai.sh
+++ yilai.sh
@@ -1,6 +1,10 @@
 ql repo https://github.com/LJMX996/jd.git "jd_|jx_" "" "JDJRValidator_Pure" "aaron" 
 
 
+cp -rf /ql/repo/LJMX996_jd_aaron/sign_graphics_validate.js /ql/scripts/
+
+cp -rf /ql/repo/LJMX996_jd_aaron/JDJRValidator_Pure.js /ql/scripts/
+
 cp -rf /ql/repo/LJMX996_jd_aaron/utils /ql/scripts/
 
 npm install axios date-fns
