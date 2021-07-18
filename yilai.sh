58d81993d4eb6f0b0e4e34722152fb5d827d0cdc
刘庆 <lq510885@qq.com>
2021-07-18 08:37:20 +0800

    Update

--- yilai.sh
+++ yilai.sh
@@ -1,9 +1,7 @@
-ql repo https://github.com/LJMX996/jd.git "jd_|jx_" "" "JDJRValidator_Pure" "aaron" 
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_" "" "JDJRValidator_Pure|sign_graphics_validate" "aaron" 
 
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_|getJDCookie" "activity|backUp|jd_delCoupon|format_" "^jd[^_]|USER" "hello"
 
-cp -rf /ql/repo/LJMX996_jd_aaron/sign_graphics_validate.js /ql/scripts/
-
-cp -rf /ql/repo/LJMX996_jd_aaron/JDJRValidator_Pure.js /ql/scripts/
 
 cp -rf /ql/repo/LJMX996_jd_aaron/utils /ql/scripts/
 
