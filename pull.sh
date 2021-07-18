8050c90f46ed0e99e128689e9e00375d80b8ed19
刘庆 <lq510885@qq.com>
2021-07-18 09:00:42 +0800

    Update

---
+++ pull.sh
@@ -0,0 +1,3 @@
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_|pull" "" "JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js" "aaron" 
+
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_|getJDCookie" "activity|backUp|jd_delCoupon|format_" "^jd[^_]|USER" "hello"
