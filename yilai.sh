ab7ae848facc11dc8afd3f86f455e7c2cf25648b
刘庆 <lq510885@qq.com>
2021-07-18 12:20:10 +0800

    依赖

--- yilai.sh
+++ yilai.sh
@@ -1,7 +1,3 @@
-ql repo https://github.com/LJMX996/jd.git "jd_|jx_" "" "JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js" "aaron" 
-
-ql repo https://github.com/LJMX996/jd.git "jd_|jx_|getJDCookie" "activity|backUp|jd_delCoupon|format_" "^jd[^_]|USER" "hello"
-
 
 cp -rf /ql/repo/LJMX996_jd_aaron/utils /ql/scripts/
 
