8177711da0c25f8d359762772c375c754e7aa43a
刘庆 <lq510885@qq.com>
2021-08-05 08:31:09 +0800

    Update

--- gua_wealth_island.js
+++ gua_wealth_island.js
@@ -13,7 +13,7 @@ const $ = new Env('财富大陆');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 // const notify = $.isNode() ? require('./sendNotify') : '';
 $.CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS;
-const UA = `jdpingou;iPhone;4.11.0;${Math.ceil(Math.random()*2+12)}.${Math.ceil(Math.random()*4)};${randomString(40)};`
+let UA = `jdapp;iPhone;10.0.5;${Math.ceil(Math.random()*2+12)}.${Math.ceil(Math.random()*4)};${randomString(40)};`
 function randomString(e) {
   e = e || 32;
   let t = "abcdefhijkmnprstwxyz2345678", a = t.length, n = "";
@@ -61,41 +61,8 @@ $.appId = 10032;
       $.index = i + 1;
       $.isLogin = true;
       console.log(`\n*****开始【京东账号${$.index}】${$.UserName}****\n`);
+      UA = `jdapp;iPhone;10.0.5;${Math.ceil(Math.random()*2+12)}.${Math.ceil(Math.random()*4)};${randomString(40)};`
       await run();
-    }
-  }
-  // 助力
-  let res = [], res2 = [];
-  $.InviteLists = []
-  if (HelpAuthorFlag) {
-    $.innerInviteList = await getAuthorShareCode('');
-    res2 = await getAuthorShareCode('');
-    $.innerInviteLists = getRandomArrayElements([...res, ...res2], [...res, ...res2].length);
-    $.InviteLists.push(...$.InviteList,...$.innerInviteList,...$.innerInviteLists);
-  }else{
-    $.InviteLists.push(...$.InviteList);
-  }
-  for (let i = 0; i < cookiesArr.length; i++) {
-    $.cookie = cookiesArr[i];
-    $.canHelp = true;
-    $.UserName = decodeURIComponent($.cookie.match(/pt_pin=([^; ]+)(?=;?)/) && $.cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-    $.index = i + 1;
-    if ($.InviteLists && $.InviteLists.length) console.log(`\n******开始【邀请好友助力】*********\n`);
-    for (let j = 0; j < $.InviteLists.length && $.canHelp; j++) {
-      $.inviteId = $.InviteLists[j];
-      console.log(`${$.UserName} 助力 ${$.inviteId}`);
-      let res = await taskGet(`story/helpbystage`, '_cfd_t,bizCode,dwEnv,ptag,source,strShareId,strZone', `&strShareId=${$.inviteId}`)
-      if(res && res.iRet == 0){
-        console.log(`助力成功: 获得${res.Data && res.Data.GuestPrizeInfo && res.Data.GuestPrizeInfo.strPrizeName || ''}`)
-      }else if(res && res.sErrMsg){
-        console.log(res.sErrMsg)
-        if(res.sErrMsg.indexOf('助力次数达到上限') > -1 || res.iRet === 2232 || res.sErrMsg.indexOf('助力失败') > -1){
-          break
-        }
-      }else{
-        console.log(JSON.stringify(res))
-      }
-      await $.wait(1000);
     }
   }
 })()
@@ -134,6 +101,8 @@ async function run() {
     await buildList()
     // 签到 邀请奖励
     await sign()
+    // 签到-小程序
+    await signs()
     // 捡垃圾
     await pickshell(1)
     // 热气球接客
@@ -367,7 +336,7 @@ async function sign(){
     
     if($.Aggrtask && $.Aggrtask.Data && $.Aggrtask.Data.Employee && $.Aggrtask.Data.Employee.EmployeeList){
         if($.Aggrtask.Data && $.Aggrtask.Data.Employee && $.Aggrtask.Data.Employee.EmployeeList){
-        console.log(`\n领取邀请奖励`)
+        console.log(`\n领取邀请奖励(${$.Aggrtask.Data.Employee.EmployeeList.length || 0}/${$.Aggrtask.Data.Employee.dwNeedTotalPeople || 0})`)
         for(let i of $.Aggrtask.Data.Employee.EmployeeList){
           if(i.dwStatus == 0){
             let res = await taskGet(`story/helpdraw`, '_cfd_t,bizCode,dwEnv,dwUserId,ptag,source,strZone', `&ptag=&dwUserId=${i.dwId}`)
@@ -380,22 +349,62 @@ async function sign(){
     $.logErr(e);
   }
 }
+// 签到-小程序
+async function signs(){
+  try{
+    // 签到-小程序
+    await $.wait(2000)
+    $.Aggrtask = await taskGet(`story/GetTakeAggrPages`, '_cfd_t,bizCode,dwEnv,ptag,source,strZone', '&ptag=')
+    if($.Aggrtask && $.Aggrtask.Data && $.Aggrtask.Data.Sign){
+      if($.Aggrtask.Data.Sign.dwTodayStatus == 0) {
+        console.log('\n签到-小程序')
+        let flag = false
+        let ddwCoin = 0
+        let ddwMoney = 0
+        let dwPrizeType = 0
+        let strPrizePool = 0
+        let dwPrizeLv = 0
+        for(i of $.Aggrtask.Data.Sign.SignList){
+          if(i.dwStatus == 0){
+            flag = true
+            ddwCoin = i.ddwCoin || 0
+            ddwMoney = i.ddwMoney || 0
+            dwPrizeType = i.dwPrizeType || 0
+            strPrizePool = i.strPrizePool || 0
+            dwPrizeLv = i.dwBingoLevel || 0
+            break;
+          }
+        }
+        if(flag){
+          let additional = `&ptag=&ddwCoin=${ddwCoin}&ddwMoney=${ddwMoney}&dwPrizeType=${dwPrizeType}&strPrizePool${strPrizePool && '='+strPrizePool ||''}&dwPrizeLv=${dwPrizeLv}`
+          let stk= `_cfd_t,bizCode,ddwCoin,ddwMoney,dwEnv,dwPrizeLv,dwPrizeType,ptag,source,strPrizePool,strZone`
+          let res = await taskGet(`story/RewardSigns`, stk, additional)
+          await printRes(res, '签到-小程序')
+        }
+      }
+    }
+  }catch (e) {
+    $.logErr(e);
+  }
+}
 // 捡垃圾
 async function pickshell(num = 1){
   return new Promise(async (resolve) => {
     try{
       console.log(`\n捡垃圾`)
-      // pickshell dwType 1珍珠 2海螺 3大海螺  4海星
+      // pickshell dwType 1珍珠 2海螺 3大海螺  4海星 5小贝壳 6扇贝
       for(i=1;num--;i++){
         await $.wait(2000)
         $.queryshell = await taskGet(`story/queryshell`, '_cfd_t,bizCode,dwEnv,ptag,source,strZone', `&ptag=`)
-        let c = 4
+        let c = 6
         for(i=1;c--;i++){
           let o = 1
           let name = '珍珠'
           if(i == 2) name = '海螺'
           if(i == 3) name = '大海螺'
           if(i == 4) name = '海星'
+          if(i == 5) name = '小贝壳'
+          if(i == 6) name = '扇贝'
           do{
             console.log(`去捡${name}第${o}次`)
             o++;
@@ -575,7 +584,7 @@ async function ActTask(){
       for(let i in $.Biztask.Data.TaskList){
         let item = $.Biztask.Data.TaskList[i]
         if(item.dwAwardStatus != 2 && item.dwCompleteNum === item.dwTargetNum) continue
-        console.log(`去做任务 ${item.strTaskName},${item.dwAwardStatus},${item.dwOrderId},${item.dwCompleteNum},${item.dwTargetNum}`)
+        console.log(`任务 ${item.strTaskName} ${item.dwCompleteNum}/${item.dwTargetNum}`)
         if (item.dwAwardStatus == 2 && item.dwCompleteNum === item.dwTargetNum) {
           res = await taskGet(`Award1`, '_cfd_t,bizCode,dwEnv,ptag,source,strZone,taskId', `&ptag=&taskId=${item.ddwTaskId}`)
           if(res.ret == 0){
@@ -592,27 +601,34 @@ async function ActTask(){
           }
           await $.wait(1000)
         }
-        if(item.dwAwardStatus == 2 && item.dwCompleteNum < item.dwTargetNum && [2].includes(item.dwOrderId)){
+        if(item.dwAwardStatus == 2 && item.dwCompleteNum < item.dwTargetNum && [1,2].includes(item.dwOrderId)){
+          await $.wait(1000)
           if(item.dwOrderId == 2){
-            let b = (item.dwTargetNum-item.dwCompleteNum)
-            // 热气球接客
-            await service(b)
-            await $.wait(1000)
-            res = await taskGet(`Award1`, '_cfd_t,bizCode,dwEnv,ptag,source,strZone,taskId', `&ptag=&taskId=${item.ddwTaskId}`)
-            if(res.ret == 0){
-              if(res.data.prizeInfo){
-                res.data.prizeInfo = $.toObj(res.data.prizeInfo)
-              }
-              if(res.data.prizeInfo.ddwCoin || res.data.prizeInfo.ddwMoney){
-                console.log(`${item.strTaskName} 领取奖励:${res.data.prizeInfo.ddwCoin && res.data.prizeInfo.ddwCoin+'金币' || ''} ${res.data.prizeInfo.ddwMoney && res.data.prizeInfo.ddwMoney+'财富' || ''}`)
-              }else{
-                console.log(`${item.strTaskName} 领取奖励:`, JSON.stringify(res))
-              }
-            }else{
-              console.log(`${item.strTaskName} 领取奖励失败:`, JSON.stringify(res))
+            if(item.strTaskName.indexOf('热气球接待') > -1){
+              let b = (item.dwTargetNum-item.dwCompleteNum)
+              // 热气球接客
+              await service(b)
+              await $.wait((Number(item.dwLookTime) * 1000) || 1000)
             }
-            await $.wait(1000)
+          }else if(item.dwOrderId == 1){
+            await $.wait((Number(item.dwLookTime) * 1000) || 1000)
+            res = await taskGet('DoTask1', '_cfd_t,bizCode,configExtra,dwEnv,ptag,source,strZone,taskId', `&ptag=&taskId=${item.ddwTaskId}&configExtra=`)
           }
+          await $.wait(1000)
+          res = await taskGet(`Award1`, '_cfd_t,bizCode,dwEnv,ptag,source,strZone,taskId', `&ptag=&taskId=${item.ddwTaskId}`)
+          if(res.ret == 0){
+            if(res.data.prizeInfo){
+              res.data.prizeInfo = $.toObj(res.data.prizeInfo)
+            }
+            if(res.data.prizeInfo.ddwCoin || res.data.prizeInfo.ddwMoney){
+              console.log(`${item.strTaskName} 领取奖励:${res.data.prizeInfo.ddwCoin && res.data.prizeInfo.ddwCoin+'金币' || ''} ${res.data.prizeInfo.ddwMoney && res.data.prizeInfo.ddwMoney+'财富' || ''}`)
+            }else{
+              console.log(`${item.strTaskName} 领取奖励:`, JSON.stringify(res))
+            }
+          }else{
+            console.log(`${item.strTaskName} 领取奖励失败:`, JSON.stringify(res))
+          }
+          await $.wait(1000)
         }
       }
     }
@@ -650,7 +666,7 @@ async function UserTask(){
           await $.wait(1000)
         }
         if(item.dateType == 2){
-          if(item.awardStatus === 2 && item.completedTimes < item.targetTimes && [1,2,3,4].includes(item.orderId)){
+          if(item.completedTimes < item.targetTimes && [1,2,3,4].includes(item.orderId)){
             if(item.taskName.indexOf('捡贝壳') >-1 || item.taskName.indexOf('赚京币任务') >-1) continue
             let b = (item.targetTimes-item.completedTimes)
             for(i=1;b--;i++){
@@ -766,6 +782,7 @@ function taskGet(type, stk, additional){
 }
 function getGetRequest(type, stk='', additional='') {
   let url = ``;
+  let dwEnv = 7;
   if(type == 'user/ComposeGameState'){
     url = `https://m.jingxi.com/jxbfd/${type}?__t=${Date.now()}&strZone=jxbfd&dwFirst=1&_=${Date.now()}&sceneval=2`
   }else if(type == 'user/RealTmReport'){
@@ -773,17 +790,23 @@ function getGetRequest(type, stk='', additional='') {
   }else{
     let stks = ''
     if(stk) stks = `&_stk=${stk}`
-    if(type == 'GetUserTaskStatusList' || type == 'Award' || type == 'Award1' || type == 'DoTask'){
+    if(type == 'story/GetTakeAggrPages' || type == 'story/RewardSigns') dwEnv = 6
+    if(type == 'story/GetTakeAggrPages') type = 'story/GetTakeAggrPage'
+    if(type == 'story/RewardSigns') type = 'story/RewardSign'
+    if(type == 'GetUserTaskStatusList' || type == 'Award' || type == 'Award1' || type == 'DoTask' || type == 'DoTask1'){
       let bizCode = 'jxbfd'
       if(type == 'Award1'){
         bizCode = 'jxbfddch'
         type = 'Award'
+      }else if(type == 'DoTask1'){
+        bizCode = 'jxbfddch'
+        type = 'DoTask'
       }
-      url = `https://m.jingxi.com/newtasksys/newtasksys_front/${type}?strZone=jxbfd&bizCode=${bizCode}&source=jxbfd&dwEnv=3&_cfd_t=${Date.now()}${additional}${stks}&_ste=1&_=${Date.now()}&sceneval=2&g_login_type=1`
+      url = `https://m.jingxi.com/newtasksys/newtasksys_front/${type}?strZone=jxbfd&bizCode=${bizCode}&source=jxbfd&dwEnv=${dwEnv}&_cfd_t=${Date.now()}${additional}${stks}&_ste=1&_=${Date.now()}&sceneval=2&g_login_type=1`
     }else if(type == 'user/ComposeGameAddProcess' || type == 'user/ComposeGameAward'){
       url = `https://m.jingxi.com/jxbfd/${type}?strZone=jxbfd&__t=${Date.now()}${additional}${stks}&_=${Date.now()}&sceneval=2`;
     }else{
-      url = `https://m.jingxi.com/jxbfd/${type}?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=${Date.now()}&ptag=${additional}${stks}&_=${Date.now()}&sceneval=2`;
+      url = `https://m.jingxi.com/jxbfd/${type}?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=${dwEnv}&_cfd_t=${Date.now()}&ptag=${additional}${stks}&_=${Date.now()}&sceneval=2`;
     }
     url += `&h5st=${decrypt(Date.now(), stk, '', url)}`;
   }
@@ -833,68 +856,71 @@ function biz(contents){
 }
 
 function opId(type){
-  let opId = 5001
-  if(type == "user/QueryUserInfo") opId = 1
-  if(type == "user/GetMgrAllConf") opId = 3
-  if(type == "story/QueryUserStory") opId = 5
-  if(type == "user/GetJdToken") opId = 11
-  if(type == "story/CouponState") opId = 13
-  if(type == "story/WelfareDraw") opId = 15
-  if(type == "story/GetWelfarePage") opId = 17
-  if(type == "story/SendWelfareMoney") opId = 19
-  if(type == "user/SetMark") opId = 23
-  if(type == "user/GetMark") opId = 25
-  if(type == "user/guideuser") opId = 27
-  if(type == "user/createbuilding") opId = 29
-  if(type == "user/BuildLvlUp") opId = 31
-  if(type == "user/CollectCoin") opId = 33
-  if(type == "user/GetBuildInfo") opId = 35
-  if(type == "user/SpeedUp") opId = 37
-  if(type == "story/AddNoticeMsg") opId = 39
-  if(type == "user/breakgoldenegg") opId = 41
-  if(type == "user/closewindow") opId = 43
-  if(type == "user/drawpackprize") opId = 45
-  if(type == "user/GetMoneyDetail") opId = 47
-  if(type == "user/EmployTourGuide") opId = 49
-  if(type == "story/sellgoods") opId = 51
-  if(type == "story/querystorageroom") opId = 53
-  if(type == "user/queryuseraccount") opId = 55
-  if(type == "user/EmployTourGuideInfo") opId = 57
-  if(type == "consume/TreasureHunt") opId = 59
-  if(type == "story/QueryAppSignList") opId = 61
-  if(type == "story/AppRewardSign") opId = 63
-  if(type == "task/addCartSkuNotEnough") opId = 123
-  if(type == "story/GetActTask") opId = 125
-  if(type == "story/ActTaskAward") opId = 127
-  if(type == "story/DelayBizReq") opId = 131
-  if(type == "story/queryshell") opId = 65
-  if(type == "story/QueryRubbishInfo") opId = 67
-  if(type == "story/pickshell") opId = 69
-  if(type == "story/CollectorOper") opId = 71
-  if(type == "story/MermaidOper") opId = 73
-  if(type == "story/RubbishOper") opId = 75
-  if(type == "story/SpecialUserOper") opId = 77
-  if(type == "story/GetUserTaskStatusList") opId = 79
-  if(type == "user/ExchangeState") opId = 87
-  if(type == "user/ExchangePrize") opId = 89
-  if(type == "user/GetRebateGoods") opId = 91
-  if(type == "user/BuyGoods") opId = 93
-  if(type == "user/UserCashOutState") opId = 95
-  if(type == "user/CashOut") opId = 97
-  if(type == "user/GetCashRecord") opId = 99
-  if(type == "user/CashOutQuali") opId = 101
-  if(type == "user/GetAwardList") opId = 103
-  if(type == "story/QueryMailBox") opId = 105
-  if(type == "story/MailBoxOper") opId = 107
-  if(type == "story/UserMedal") opId = 109
-  if(type == "story/QueryMedalList") opId = 111
-  if(type == "story/GetTakeAggrPage") opId = 113
-  if(type == "story/GetTaskRedDot") opId = 115
-  if(type == "story/RewardSign") opId = 117
-  if(type == "story/helpdraw") opId = 119
-  if(type == "story/helpbystage") opId = 121
-  if(type == "story/AddSuggest") opId = 133
-  return opId
+  let jsonMap = {
+    "user/QueryUserInfo": 1,
+    "user/GetMgrAllConf": 3,
+    "story/QueryUserStory": 5,
+    "user/GetJdToken": 11,
+    "story/CouponState": 13,
+    "story/WelfareDraw": 15,
+    "story/GetWelfarePage": 17,
+    "story/SendWelfareMoney": 19,
+    "user/SetMark": 23,
+    "user/GetMark": 25,
+    "user/guideuser": 27,
+    "user/createbuilding": 29,
+    "user/BuildLvlUp": 31,
+    "user/CollectCoin": 33,
+    "user/GetBuildInfo": 35,
+    "user/SpeedUp": 37,
+    "story/AddNoticeMsg": 39,
+    "user/breakgoldenegg": 41,
+    "user/closewindow": 43,
+    "user/drawpackprize": 45,
+    "user/GetMoneyDetail": 47,
+    "user/EmployTourGuide": 49,
+    "story/sellgoods": 51,
+    "story/querystorageroom": 53,
+    "user/queryuseraccount": 55,
+    "user/EmployTourGuideInfo": 57,
+    "consume/TreasureHunt": 59,
+    "story/QueryAppSignList": 61,
+    "story/AppRewardSign": 63,
+    "story/queryshell": 65,
+    "story/QueryRubbishInfo": 67,
+    "story/pickshell": 69,
+    "story/CollectorOper": 71,
+    "story/MermaidOper": 73,
+    "story/RubbishOper": 75,
+    "story/SpecialUserOper": 77,
+    "story/GetUserTaskStatusList": 79,
+    "user/ExchangeState": 87,
+    "user/ExchangePrize": 89,
+    "user/GetRebateGoods": 91,
+    "user/BuyGoods": 93,
+    "user/UserCashOutState": 95,
+    "user/CashOut": 97,
+    "user/GetCashRecord": 99,
+    "user/CashOutQuali": 101,
+    "user/GetAwardList": 103,
+    "story/QueryMailBox": 105,
+    "story/MailBoxOper": 107,
+    "story/UserMedal": 109,
+    "story/QueryMedalList": 111,
+    "story/GetTakeAggrPage": 113,
+    "story/GetTaskRedDot": 115,
+    "story/RewardSign": 117,
+    "story/helpdraw": 119,
+    "story/helpbystage": 121,
+    "task/addCartSkuNotEnough": 123,
+    "story/GetActTask": 125,
+    "story/ActTaskAward": 127,
+    "story/DelayBizReq": 131,
+    "story/AddSuggest": 133,
+  }
+  let opId = jsonMap[type]
+  if (opId!=undefined) return opId
+  return 5001
 }
 
 async function requestAlgo() {
