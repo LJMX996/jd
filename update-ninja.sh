a90f8e384996fffef308c17cc181d3a696b9581d
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2021-08-06 22:58:38 +0800

    Update update-ninja.sh

--- update-ninja.sh
+++ update-ninja.sh
@@ -4,3 +4,8 @@ git pull
 pnpm install
 pm2 start
 cp sendNotify.js /ql/scripts/sendNotify.js
+cd /ql/ninja/backend/static
+rm -rf index.html
+rm -rf push.jpg
+wget https://raw.githubusercontent.com/LJMX996/jd/help/index.html
+wget https://raw.githubusercontent.com/LJMX996/jd/help/push.jpg
