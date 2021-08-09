89a8318f99c72432f05f1031c6a224f42c6b8724
刘庆 <lq510885@qq.com>
2021-08-10 01:01:14 +0800

    Update

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
-ql repo https://github.com/LJMX996/jd.git "jd_|jx_|pull|gua_" "tmp" "JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js|ZooFaker_Necklace.js" "aaron" 
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_|pull|gua_|code_" "tmp" "JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js|ZooFaker_Necklace.js" "aaron" 
 
 ql repo https://github.com/LJMX996/jd.git "jd_|jx_|getJDCookie" "tools|activity|backUp|jd_delCoupon|format_" "^jd[^_]|USER" "hello"
 
