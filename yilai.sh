7ef499a93e0b6bc9b5d6b34c7073fd28baf51dc1
刘庆 <lq510885@qq.com>
2021-08-14 22:01:26 +0800

    Update

--- yilai.sh
+++ yilai.sh
@@ -1,14 +1,25 @@
 
 cp -rf /ql/repo/LJMX996_jd_aaron/utils /ql/scripts/
 
-npm install got
-npm install tough-cookie
-npm install axios date-fns
-npm install crypto-js
-npm install download
 
+cd /ql/scripts && npm install got
+cd /ql/scripts && npm install tough-cookie
+cd /ql/scripts && npm install axios date-fns
+cd /ql/scripts && npm install crypto-js
+cd /ql/scripts && npm install download
+cd /ql/scripts && npm install -g typescript
 cd /ql/scripts && npm i -S png-js
 
-apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd /ql/scripts && npm install canvas --build-from-source
-apk add libc6-compat
-apk add nodejs-current
+
+
+cd /ql/scripts && apk add libc6-compat
+cd /ql/scripts && apk add nodejs-current
+
+
+cd /ql/scripts && pip3 install requests
+
+
+cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
+
+
+cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
