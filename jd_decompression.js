cb20b83d6a736113347b391c5998a7d6f8dd555b
刘庆 <lq510885@qq.com>
2021-08-14 08:20:51 +0800

    Update

--- jd_decompression.js
+++ jd_decompression.js
@@ -1,6 +1,7 @@
 /**
- * 活动到24号。一天可以跑2次
-5 4,6,8,10 12-24 8 * jd_decompression.js
+蚊子腿豆子，24号应该可以参与瓜分
+活动到24号。一天可以跑2次
+cron 5 6,8 12-24 8 *  https://raw.githubusercontent.com/star261/jd/main/scripts/jd_decompression.js
  */
 const $ = new Env('热血心跳,狂解压');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
@@ -8,14 +9,18 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [];
 $.activityID = 'dz2107100008586804';
 $.shopid = '1000085868';
-$.shareUuid = '1f9d7c54cd654088a32bab86de9bba95';
+const inCode= ['1f9d7c54cd654088a32bab86de9bba95'];
+$.shareUuid = getRandomArrayElements(inCode,1)[0];
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
         cookiesArr.push(jdCookieNode[item])
     })
     if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
 } else {
-    cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
+    cookiesArr = [
+        $.getdata("CookieJD"),
+        $.getdata("CookieJD2"),
+        ...$.toObj($.getdata("CookiesJD") || "[]").map((item) => item.cookie)].filter((item) => !!item);
 }
 !(async () => {
     if (!cookiesArr[0]) {
@@ -85,13 +90,15 @@ async function main() {
         console.log(`获取活动详情失败`);return;
     }
     console.log(`获取活动详情成功`);
+    //console.log(`助力码：${$.activityData.actorUuid}`);
     await doTask();
     await $.wait(3000);
     await takePostRequest('activityContent');
     let score = $.activityData.score;
     console.log(`可投票次数：`+score);
     let scoreFlag = false;
-    for (let i = 0; i < score; i++) {
+    $.canScore = true;
+    for (let i = 0; i < score && $.canScore; i++) {
         scoreFlag = true;
         console.log(`进行第${i+1}次投票`);
         await takePostRequest('insxintiao');
@@ -113,7 +120,19 @@ async function main() {
         await takePostRequest('draw');
         await $.wait(1500);
     }
-
+    if($.shareUuid === '1f9d7c54cd654088a32bab86de9bba95'){
+        $.shareUuid = $.activityData.actorUuid;
+    }
+}
+function getRandomArrayElements(arr, count) {
+    var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
 }
 
 async function doTask(){
@@ -254,7 +273,12 @@ function dealReturn(type, data) {
             console.log(JSON.stringify(data))
             break;
         case 'insxintiao':
-            console.log(JSON.stringify(data))
+            if (data.result === true && data.count === 0) {
+
+            }else{
+                $.canScore = false;
+            }
+            console.log(JSON.stringify(data));
             break;
         case 'draw':
             if (data.result === true && data.count === 0) {
@@ -269,6 +293,12 @@ function dealReturn(type, data) {
             } else {
                 //console.log(JSON.stringify(data))
             }
+            console.log(JSON.stringify(data))
+            break;
+        case 'insertCrmPageVisit':
+            console.log(JSON.stringify(data))
+            break;
+        case 'getSimpleActInfoVo':
             console.log(JSON.stringify(data))
             break;
         default:
