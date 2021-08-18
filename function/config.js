f0975eba179fa01039760ec9bbbdb2c9ff7dcd5b
刘庆 <lq510885@qq.com>
2021-08-18 18:53:15 +0800

    Update

---
+++ function/config.js
@@ -0,0 +1,1 @@
+module.exports = {"ThreadJs":[],"invokeKey":"ztmFUCxcPMNyUq0P"}
--- pull.sh
+++ pull.sh
@@ -1,7 +1,9 @@
-ql repo https://github.com/LJMX996/jd.git "jd_|jx_|pull|gua_|code_|zy_" "tmp" "JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js|ZooFaker_Necklace.js|USER" "aaron" 
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_|pull|gua_|code_|zy_|jd-task-" "tmp" "JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js|ZooFaker_Necklace.js|USER" "aaron" 
 
 ql repo https://github.com/LJMX996/jd.git "jd_|jx_|getJDCookie" "tools|activity|backUp|jd_delCoupon|format_" "^jd[^_]" "hello"
 
 cp -rf /ql/repo/LJMX996_jd_aaron/utils /ql/scripts/
+
+cp -rf /ql/repo/LJMX996_jd_aaron/function /ql/scripts/
 
 cp /ql/ninja/backend/sendNotify.js /ql/scripts/sendNotify.js
