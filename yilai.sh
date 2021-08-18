efdb8e5c8af8889508a157926f92414541131b80
刘庆 <lq510885@qq.com>
2021-08-18 18:56:04 +0800

    Update

--- yilai.sh
+++ yilai.sh
@@ -1,7 +1,7 @@
 
 cp -rf /ql/repo/LJMX996_jd_aaron/utils /ql/scripts/
 
-
+cd /ql/scripts && npm install jsdom
 cd /ql/scripts && npm install got
 cd /ql/scripts && npm install tough-cookie
 cd /ql/scripts && npm install axios date-fns
