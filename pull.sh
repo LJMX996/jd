3d8644d5915e033b5a79a0e35fe0a1dcb22f00dc
刘庆 <lq510885@qq.com>
2021-08-21 20:09:15 +0800

    Update

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
-ql repo https://github.com/LJMX996/jd.git "jd_|jx_|gua_|code_|zy_|jd-task-" "tmp" "pull.sh|JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js|ZooFaker_Necklace.js|USER" "aaron" 
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_|gua_|code_|zy_|jd-task-" "tmp" "JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js|ZooFaker_Necklace.js|USER" "aaron" 
 
 ql repo https://github.com/LJMX996/jd.git "jd_|jx_|getJDCookie" "tools|activity|backUp|jd_delCoupon|format_" "^jd[^_]" "hello"
 
@@ -7,4 +7,7 @@ cp -rf /ql/repo/LJMX996_jd_aaron/utils /ql/scripts/
 cp -rf /ql/repo/LJMX996_jd_aaron/function /ql/scripts/
 
 cp /ql/ninja/backend/sendNotify.js /ql/scripts/sendNotify.js
+
+
+cp -rf /ql/repo/LJMX996_jd_aaron/pull.sh /ql/scripts/
 
