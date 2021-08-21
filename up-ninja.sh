6a257ab09cfaa1910d04efcad2eb622703ead856
刘庆 <lq510885@qq.com>
2021-08-21 19:36:52 +0800

    Update

--- up-ninja.sh
+++ up-ninja.sh
@@ -5,9 +5,9 @@ git pull
 rm -rf /ql/ninja/backend/ql.js
 rm -rf /ql/ninja/backend/user.js
 
-cd ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/ql.js
+cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/ql.js
 
-cd ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/user.js
+cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/user.js
 
 
 pnpm install
