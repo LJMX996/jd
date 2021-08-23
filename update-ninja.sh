95abe5de56981097f164abd1a46081f5c2b1a507
刘庆 <lq510885@qq.com>
2021-08-23 20:14:19 +0800

    Update

--- update-ninja.sh
+++ update-ninja.sh
@@ -1,6 +1,8 @@
 cd /ql/ninja/backend
+
 rm -rf .env
 wget https://raw.githubusercontent.com/LJMX996/jd/help/.env
+
 git checkout .
 git pull
 
@@ -11,11 +13,14 @@ cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja
 
 cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/user.js
 
-pnpm install
-pm2 start
 cp sendNotify.js /ql/scripts/sendNotify.js
 cd /ql/ninja/backend/static
 rm -rf index.html
 rm -rf push.jpg
 wget https://raw.githubusercontent.com/LJMX996/jd/help/index.html
 wget https://raw.githubusercontent.com/LJMX996/jd/help/push.jpg
+
+cd /ql/ninja/backend
+pnpm install
+pm2 start
+
