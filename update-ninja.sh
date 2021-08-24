733e661aeb77cf6b533c6291a2ae707d5cfc6625
刘庆 <lq510885@qq.com>
2021-08-24 09:29:42 +0800

    Update

--- update-ninja.sh
+++ update-ninja.sh
@@ -8,10 +8,13 @@ git pull
 
 rm -rf /ql/ninja/backend/ql.js
 rm -rf /ql/ninja/backend/user.js
+rm -rf /ql/ninja/backend/utils/USER_AGENT.js
 
 cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/ql.js
 
 cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/user.js
+
+cd /ql/ninja/backend/utils/ && wget https://raw.githubusercontent.com/LJMX996/jd/help/USER_AGENT.js
 
 cp sendNotify.js /ql/scripts/sendNotify.js
 cd /ql/ninja/backend/static
