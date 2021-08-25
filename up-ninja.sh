9543d64d63869cd2b3034e5d7429462a4825764c
刘庆 <lq510885@qq.com>
2021-08-25 08:22:29 +0800

    Update

--- up-ninja.sh
+++ up-ninja.sh
@@ -1,19 +1,31 @@
 cd /ql/ninja/backend
+
+
 git checkout .
 git pull
 
 rm -rf /ql/ninja/backend/ql.js
 rm -rf /ql/ninja/backend/user.js
+rm -rf /ql/ninja/backend/utils/USER_AGENT.js
 
 cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/ql.js
 
 cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/user.js
 
+cd /ql/ninja/backend/utils/ && wget https://raw.githubusercontent.com/LJMX996/jd/help/USER_AGENT.js
 
+
+cd /ql/ninja/backend
 pnpm install
 pm2 start
+
 cp sendNotify.js /ql/scripts/sendNotify.js
+
 cd /ql/ninja/backend/static
 rm -rf index.html
-wget http://ghproxy.com/https://raw.githubusercontent.com/LJMX996/jd/help/index.html
+
+wget https://raw.githubusercontent.com/LJMX996/jd/help/index.html
+
+
+
 
