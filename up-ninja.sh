3f7ebce1c631575869c97bee88434bb9d0f94707
刘庆 <lq510885@qq.com>
2021-08-27 13:08:43 +0800

    Update

--- up-ninja.sh
+++ up-ninja.sh
@@ -4,24 +4,18 @@ cd /ql/ninja/backend
 git checkout .
 git pull
 
-
 rm -rf /ql/ninja/backend/utils/USER_AGENT.js
-
 
 cd /ql/ninja/backend/utils/ && wget https://raw.githubusercontent.com/LJMX996/jd/help/USER_AGENT.js
 
+rm -rf /ql/ninja/backend/static/*
+cp -rf /ql/repo/LJMX996_jd_aaron/static/* /ql/ninja/backend/static/
 
 cd /ql/ninja/backend
 pnpm install
 pm2 start
 
 cp sendNotify.js /ql/scripts/sendNotify.js
-
-cd /ql/ninja/backend/static
-rm -rf index.html
-
-wget https://raw.githubusercontent.com/LJMX996/jd/help/index.html
-
 
 
 
