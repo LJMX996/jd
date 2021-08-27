b7cc5acd2962dea8d8ef4c1fbfd42692cce64048
刘庆 <lq510885@qq.com>
2021-08-27 12:58:53 +0800

    Update

--- update-ninja.sh
+++ update-ninja.sh
@@ -10,6 +10,8 @@ rm -rf /ql/ninja/backend/utils/USER_AGENT.js
 
 cd /ql/ninja/backend/utils/ && wget https://raw.githubusercontent.com/LJMX996/jd/help/USER_AGENT.js
 
+rm -rf /ql/ninja/backend/static/*
+cp -rf /ql/repo/LJMX996_jd_aaron/static/* /ql/ninja/backend/static/
 
 cd /ql/ninja/backend
 pnpm install
@@ -18,9 +20,9 @@ pm2 start
 cp sendNotify.js /ql/scripts/sendNotify.js
 
 cd /ql/ninja/backend/static
-rm -rf index.html
+
 rm -rf push.jpg
-wget https://raw.githubusercontent.com/LJMX996/jd/help/index.html
+
 wget https://raw.githubusercontent.com/LJMX996/jd/help/push.jpg
 
 
