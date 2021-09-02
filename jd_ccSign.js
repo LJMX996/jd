dac5406daa027433fc3ff3fadaa4939aa323db5e
刘庆 <lq510885@qq.com>
2021-09-02 20:28:38 +0800

    Update

--- jd_ccSign.js
+++ jd_ccSign.js
@@ -155,7 +155,7 @@ function getSign(functionid, body, uuid) {
     let HostArr = ['jdsign.cf', 'signer.nz.lu']
     let Host = HostArr[Math.floor((Math.random() * HostArr.length))]
     let options = {
-      url: `https://cdn.jdsign.cf/ddo`,
+      url: `https://cdn.xia.me/ddo`,
       body: JSON.stringify(data),
       headers: {
         Host,
@@ -186,7 +186,7 @@ function getsecretPin(pin) {
     let HostArr = ['jdsign.cf', 'signer.nz.lu']
     let Host = HostArr[Math.floor((Math.random() * HostArr.length))]
     let options = {
-      url: `https://cdn.jdsign.cf/pin`,
+      url: `https://cdn.xia.me/pin`,
       body: JSON.stringify(data),
       headers: {
         Host,
--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
-ql repo https://github.com/LJMX996/jd.git "jd_|jx_|gua_|code_|zy_|jd-task-|jddj_" "tmp|static" "JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js|ZooFaker_Necklace.js|USER" "aaron" 
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_|gua_|code_|zy_|jd-task-|jddj_|ql_" "tmp|static" "JDJRValidator_Pure.js|sign_graphics_validate.js|MovementFaker.js|ZooFaker_Necklace.js|USER" "aaron" 
 
 
 
