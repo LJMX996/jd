b826973181ffbd2ee8d3c69f3768c160783178a0
刘庆 <lq510885@qq.com>
2021-10-09 23:13:20 +0800

    Update js

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -272,8 +272,8 @@ if ($.isNode()) {
 	}
 
 	if (intPerSent > 0) {
-		console.log("分段通知还剩下" + cookiesArr.length % intPerSent + "个账号需要发送...");
-		if (cookiesArr.length % intPerSent != 0) {
+		//console.log("分段通知还剩下" + cookiesArr.length % intPerSent + "个账号需要发送...");
+		if (allMessage || allMessageMonth) {
 			console.log("分段通知收尾，处理发送通知....");
 			if ($.isNode() && allMessage) {
 				await notify.sendNotify(`${$.name}`, `${allMessage}`, {
@@ -459,9 +459,7 @@ async function showMsg() {
 		}
 
 	}
-	if ($.errorMsg)
-		ReturnMessage += `\n【数据报错】获取京豆数据异常!`;
-
+	
 	ReturnMessage += `\n【今日京豆】收${$.todayIncomeBean}豆`;
 
 	if ($.todayOutcomeBean != 0) {
