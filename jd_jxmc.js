4b10b685d96d6699df48ee1062a10e18008fc284
刘庆 <lq510885@qq.com>
2021-10-28 17:07:43 +0800

    gua_1111RedEnvelope.js

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -395,7 +395,7 @@ async function doTask(){
             }else if(oneTask.taskType === 4){
                 if(oneTask.awardStatus === 2 && oneTask.completedTimes === oneTask.targetTimes){
                     console.log(`完成任务：${oneTask.taskName}`);
-                    outer:for (let i = 0; i < 10; i++) {
+                    outer:for (let i = 0; i <= 10; i++) {
                         awardInfo = await takeRequest(`newtasksys`,`newtasksys_front/Award`,`source=jxmc&taskId=${oneTask.taskId}&bizCode=jxmc`,`bizCode%2Csource%2CtaskId`,true);
                         if (awardInfo.awardStatus == 0 || "" === awardInfo.prizeInfo) {
                             break outer;
