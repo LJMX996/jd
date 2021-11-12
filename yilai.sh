750760a62b663afc19d307ab6dac7deee80fb95b
刘庆 <lq510885@qq.com>
2021-11-12 16:36:28 +0800

    yilai.sh

--- yilai.sh
+++ yilai.sh
@@ -1,27 +1,38 @@
-pip3 install requests
-#pip3 install aiohttp
-pip3 install jieba
+cd /ql/scripts && pip3 install requests
+cd /ql/scripts && pip3 install --upgrade pip
+cd /ql/scripts && pip3 install jieba
+#cd /ql/scripts && pip3 install aiohttp
 
 
-apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev libc6-compat nodejs-current
+cd /ql/scripts && apk add libc6-compat
+cd /ql/scripts && apk add nodejs-current
+cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev    
+cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
 
-apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
+
+cd /ql/scripts && npm install -g pnpm
+cd /ql/scripts && pnpm install -g @otplib/preset-default
+cd /ql/scripts && pnpm install -g js-base64
+cd /ql/scripts && pnpm install -g fund
+cd /ql/scripts && pnpm install -g jsdom
+cd /ql/scripts && pnpm install -g form-data
+cd /ql/scripts && pnpm install -g tough-cookie
+cd /ql/scripts && pnpm install -g axios date-fns
+cd /ql/scripts && pnpm install -g crypto-js
+cd /ql/scripts && pnpm install -g crypto -g
+cd /ql/scripts && pnpm install -g download
+cd /ql/scripts && pnpm install -g typescript
+cd /ql/scripts && pnpm install -g png-js
+
+cd /ql/scripts && pnpm install -g got@11
+cd /ql/scripts && pnpm install -g ts-node
+cd /ql/scripts && pnpm install -g @types/node
+cd /ql/scripts && pnpm install -g canvas
 
 
-npm install -g pnpm
-pnpm install -g @otplib/preset-default
-pnpm install -g js-base64
-pnpm install -g fund
-pnpm install -g jsdom
-pnpm install -g form-data
-pnpm install -g tough-cookie
-pnpm install -g axios date-fns
-pnpm install -g crypto-js
-pnpm install -g crypto -g
-pnpm install -g download
-pnpm install -g typescript
-pnpm install -g png-js
-pnpm install -g got@11
-pnpm install -g ts-node
-pnpm install -g @types/node
-pnpm install -g canvas
+
+
+
+
+
+
