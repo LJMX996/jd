985dd29d4541fe61181e8b4823cd6d660fb72065
信条 <lq510882@qq.com>
2021-11-25 15:00:37 +0800

    外卖.js

--- tmp/外卖.js
+++ tmp/外卖.js
@@ -1,9 +1,10 @@
 // [rule: 外卖]
 // [rule: 饿了么]
+// [rule: 美团]
 // [cron: 10 7,11,17 * * *]
 var ele = "https://raw.githubusercontent.com/LJMX996/jd/aaron/tmp/elm.png";
 //图文消息
-var content = image(ele) + "\n到饭点啦，微信扫码领饿了么20元无门槛饭票！！！";
+var content = image(ele) + "\n到饭点啦，微信扫码领饭票了！！！";
 var imType = ImType();
 if (imType == "fake") {
     var groups = [{
