64ba38203e02c0a3827e0beb27ce3f35d9b73516
信条 <lq510882@qq.com>
2021-12-16 23:09:40 +0800

    qbang.js

---
+++ sillyGirl/develop/replies/qbang.js
@@ -0,0 +1,31 @@
+// Q绑查询 熊猫制作2021-12-10 QQ:1305759960 网站：bbs.xm08.cn
+// [rule: Q绑查询 ?]
+// [admin: false] 这里默认关闭了、开启true后除管理员账号外、群员不可查
+function main() {
+    var userID = GetUserID()
+    var time = (60) //禁言的时间 单位/秒
+    var qq = encodeURI(param(1))
+    if (qq == 机器人QQ号) {
+        sendText("查我？拉出去毙了") //这里设置机器人QQ、触发限制查询机器人QQ
+        return
+    }
+    if (qq == 管理员QQ号) {
+        sendText("不能查群主，禁言一分钟作为惩罚") //这里设置群主QQ、触发限制查询群主、并触发禁言1分钟
+        sleep(800)
+        GroupBan(userID, time)
+        return
+    }
+    var data = request({
+        url: "http://ovocc.top/chuanapi/chabangapi2.php?qq=" + qq ,
+        "dataType": "json"
+    })
+    var phone = (data.phone)
+    var phonediqu = (data.phonediqu)
+    var lol = (data.lol)
+    var wb = (data.wb)
+    sendText("手机号："+phone+"\n归属地："+phonediqu+"\nLOL："+lol+"\n微博："+wb)
+    
+       
+}
+    
+main()
