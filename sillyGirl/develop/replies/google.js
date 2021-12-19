20edc5c90d2bf7901a7068b9b008f9efa79913d0
信条 <lq510882@qq.com>
2021-12-19 16:30:33 +0800

    google.js

---
+++ sillyGirl/develop/replies/google.js
@@ -0,0 +1,30 @@
+//[rule: google ?]
+//[rule: gg ?]
+//[rule: 谷歌 ?]
+
+function main() {
+     var keyword = param(1)
+     var data = request({
+          method: 'GET',
+          url: 'https://google-search3.p.rapidapi.com/api/v1/search/q=' + encodeURI(keyword) + '&num=100',
+          useProxy: true,//使用代理，自动调用tg代理
+          dataType: "json",
+          headers: {
+               'x-user-agent': 'desktop',
+               'x-proxy-location': 'US',
+               'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+               'x-rapidapi-key': 'cd8518b54amsh4f14b15d452c84ap18022cjsncec7f3f236a1'//需要自己去申请，https://rapidapi.com/apigeek/api/google-search3/
+          }
+
+     })
+     if (!data.total) {
+          return "找不到" + keyword + "相关结果。"
+     }
+     sendText("找到" + data.total + "个结果，回复“n”将依次为你展示。")
+     while (input() == "n") {
+          var result = data.results.shift()
+          sendText("标题：" + result.title + "\n" + "内容：" + result.description + "\n" + "链接：" + result.link)
+     }
+}
+
+main()
