51632d7b1eabf8c13a3f14db2b58968f10d91939
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-02-17 11:01:57 +0800

    fufei.js

--- sillyGirl/develop/replies/fufei.js
+++ sillyGirl/develop/replies/fufei.js
@@ -3,7 +3,7 @@
 // [rule: 上付费车]
 // [rule: 付款码]
 // [rule: 续费]
-var qun = "https://s2.loli.net/2022/02/04/vsOiZeuW5mMqarK.jpg";
+var qun = "https://s2.loli.net/2022/02/17/ZmPNLftrFhKvxaV.jpg";
 var money = "https://s2.loli.net/2022/02/04/pmHrMBW6AnPU4Q5.jpg";
 //图文消息
 var content = image(qun) + image(money) + "\n目前付费车助力直接跑满。\n相比之前的均等助力，\n 目前助力翻了3-5倍。\n 价格提高到10¥/月。\n 转账备注自己账号用户名(不是昵称，是京东设置里面的，长按可以复制的)。\n 用自助上车的号，上付费车可以直接私聊管理。\n http://47.100.44.200:9999进去登录以后，复制ck私聊发给管理，ck有效期一个月，那就每次转一个月的";
