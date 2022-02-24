8d4d8445f06a28e18d7e657f4746b865016a8c92
xiaohan896 <80369996+xiaohan896@users.noreply.github.com>
2022-02-24 13:37:03 +0800

    Update jd_beauty.js

--- jd_beauty.js
+++ jd_beauty.js
@@ -4,8 +4,12 @@
 https://raw.githubusercontent.com/aTenb/jdOpenSharePicker/master/jd_beautyStudy.js
 更新时间:2021-12-03
 活动入口：京东app首页-美妆馆-底部中间按钮
+已支持IOS双京东账号,Node.js支持N个京东账号
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+============Quantumultx===============
 [task_local]
-30 8,12,19 * * * jd_beautyStudy.js, tag=美丽研究院, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+#美丽研究院
+30 8,12,19 * * * jd_beauty.js, tag=美丽研究院, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
  */
 const $ = new Env('美丽研究院');
 const notify = $.isNode() ? require('./sendNotify') : '';
