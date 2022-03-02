31e0f5e352aea35a98723caf5411ce23cafeba23
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-03-02 22:41:03 +0800

    jd wish

--- yilai2.sh
+++ yilai2.sh
@@ -1,36 +1,36 @@
-cd /ql/yilai && pip3 install requests
-cd /ql/yilai && pip3 install --upgrade pip
-cd /ql/yilai && pip3 install jieba
-cd /ql/yilai && pip3 install telethon
-cd /ql/yilai && pip3 install qrcode
-cd /ql/yilai && pip3 install PyExecJS
+cd /ql/scripts && pip3 install requests
+cd /ql/scripts && pip3 install --upgrade pip
+cd /ql/scripts && pip3 install jieba
+cd /ql/scripts && pip3 install telethon
+cd /ql/scripts && pip3 install qrcode
+cd /ql/scripts && pip3 install PyExecJS
 #cd /ql/scripts && pip3 install aiohttp
 
 
-cd /ql/yilai && apk add libc6-compat
-cd /ql/yilai && apk add nodejs-current
-cd /ql/yilai && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev    
-cd /ql/yilai/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
+cd /ql/scripts && apk add libc6-compat
+cd /ql/scripts && apk add nodejs-current
+cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev    
+cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
 
 
-cd /ql/yilai && npm install -g pnpm
-cd /ql/yilai && pnpm install -g @otplib/preset-default
-cd /ql/yilai && pnpm install -g js-base64
-cd /ql/yilai && pnpm install -g fund
-cd /ql/yilai && pnpm install -g jsdom
-cd /ql/yilai && pnpm install -g form-data
-cd /ql/yilai && pnpm install -g tough-cookie
-cd /ql/yilai && pnpm install -g axios date-fns
-cd /ql/yilai && pnpm install -g crypto-js
-cd /ql/yilai && pnpm install -g crypto -g
-cd /ql/yilai && pnpm install -g download
-cd /ql/yilai && pnpm install -g typescript
-cd /ql/yilai && pnpm install -g png-js
+cd /ql/scripts && npm install -g pnpm
+cd /ql/scripts && pnpm install -g @otplib/preset-default
+cd /ql/scripts && pnpm install -g js-base64
+cd /ql/scripts && pnpm install -g fund
+cd /ql/scripts && pnpm install -g jsdom
+cd /ql/scripts && pnpm install -g form-data
+cd /ql/scripts && pnpm install -g tough-cookie
+cd /ql/scripts && pnpm install -g axios date-fns
+cd /ql/scripts && pnpm install -g crypto-js
+cd /ql/scripts && pnpm install -g crypto -g
+cd /ql/scripts && pnpm install -g download
+cd /ql/scripts && pnpm install -g typescript
+cd /ql/scripts && pnpm install -g png-js
 
-cd /ql/yilai && pnpm install -g got@11
-cd /ql/yilai && pnpm install -g ts-node
-cd /ql/yilai && pnpm install -g @types/node
-cd /ql/yilai && pnpm install -g canvas
+cd /ql/scripts && pnpm install -g got@11
+cd /ql/scripts && pnpm install -g ts-node
+cd /ql/scripts && pnpm install -g @types/node
+cd /ql/scripts && pnpm install -g canvas
 
 
 
