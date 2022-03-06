1ef0c7968adfd0f6277d9368badbae1f76471d7f
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-03-06 12:07:08 +0800

    pull_kedaya.sh

---
+++ pull_kedaya.sh
@@ -0,0 +1,24 @@
+rm -rf /ql/repo/qitoqito_kedaya
+
+ql repo https://github.com/qitoqito/kedaya.git kedaya
+
+cp -a /ql/repo/qitoqito_kedaya/. /ql/scripts
+
+cp -rfv /ql/repo/qitoqito_kedaya/jw_help_distributeBean.js /ql/scripts/jw_help_distributeBean_1.js
+
+cp -rfv /ql/repo/qitoqito_kedaya/jw_help_distributeBean.js /ql/scripts/jw_help_distributeBean_2.js
+
+cp -rfv /ql/repo/qitoqito_kedaya/jw_help_distributeBean.js /ql/scripts/jw_help_distributeBean_3.js
+
+cp -rfv /ql/repo/qitoqito_kedaya/jw_help_distributeBean.js /ql/scripts/jw_help_distributeBean_4.js
+
+cp -rfv /ql/repo/qitoqito_kedaya/jw_help_distributeBean.js /ql/scripts/parse/jd/jw_help_distributeBean_1.js
+
+cp -rfv /ql/repo/qitoqito_kedaya/jw_help_distributeBean.js /ql/scripts/parse/jd/jw_help_distributeBean_2.js
+
+cp -rfv /ql/repo/qitoqito_kedaya/jw_help_distributeBean.js /ql/scripts/parse/jd/jw_help_distributeBean_3.js
+
+cp -rfv /ql/repo/qitoqito_kedaya/jw_help_distributeBean.js /ql/scripts/parse/jd/jw_help_distributeBean_4.js
+
+task qitoCreat.js now
+
