745543cf1dd52943c8d61f5d12c5b02f75383902
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-03-22 09:49:29 +0800

    install_diybot.sh

---
+++ dockerbot/install_diybot.sh
@@ -0,0 +1,13 @@
+cd /ql/repo && git clone -b aaron https://github.com/LJMX996/jd.git 
+
+cd /ql/repo/jd && git pull
+
+cp -rfv /ql/repo/jd/dockerbot/ /ql/repo/
+
+cp -rfv /ql/repo/jd/dockerbot/jbot/bot /ql/jbot/
+
+ql bot
+
+cp -rfv /ql/repo/jd/dockerbot/ /ql/repo/
+
+cp -rfv /ql/repo/jd/dockerbot/jbot/bot /ql/jbot/
