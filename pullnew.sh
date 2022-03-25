e124dcd88225289cdcb8287a3b55936ff3b24176
465243525@qq.com <Ming13750317023>
2022-03-25 11:08:59 +0800

    新版ql 拉库+依赖文件

---
+++ pullnew.sh
@@ -0,0 +1,23 @@
+#cron 20 * * * * pull.sh
+# qinglong 11.12.0+ 使用
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_|gua_|zy_|jd-task-|jddj_|ql_|ck_" "util|utils|static|tools|activity|backUp|jd_delCoupon|format_|tmp|cdle|helpjs|tmpcron|sillyGirl|dockerbot" "" "aaron" 
+
+
+
+cp -rfv /ql/data/repo/LJMX996_jd_aaron/* /ql/data/scripts/
+
+cp -rfv /ql/data/repo/LJMX996_jd_aaron/* /ql/data/scripts/LJMX996_jd_aaron/
+
+cp -rfv /ql/data/repo/LJMX996_jd_aaron/pull.sh /ql/data/config/
+
+cp -rfv /ql/data/repo/LJMX996_jd_aaron/sendNotify.js /ql/data/deps/
+
+
+
+
+
+
+
+
+
+
---
+++ yilainew.sh
@@ -0,0 +1,42 @@
+# qinglong 11.12.0+ 使用
+cd /ql/data/scripts && pip3 install requests
+cd /ql/data/scripts && pip3 install --upgrade pip
+cd /ql/data/scripts && pip3 install jieba
+cd /ql/data/scripts && pip3 install telethon
+cd /ql/data/scripts && pip3 install qrcode
+cd /ql/data/scripts && pip3 install PyExecJS
+#cd /ql/data/scripts && pip3 install aiohttp
+
+
+cd /ql/data/scripts && apk add libc6-compat
+cd /ql/data/scripts && apk add nodejs-current
+cd /ql/data/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
+cd /ql/data/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
+
+
+cd /ql/data/scripts && npm install -g pnpm
+cd /ql/data/scripts && pnpm install -g @otplib/preset-default
+cd /ql/data/scripts && pnpm install -g js-base64
+cd /ql/data/scripts && pnpm install -g fund
+cd /ql/data/scripts && pnpm install -g jsdom
+cd /ql/data/scripts && pnpm install -g form-data
+cd /ql/data/scripts && pnpm install -g tough-cookie
+cd /ql/data/scripts && pnpm install -g axios date-fns
+cd /ql/data/scripts && pnpm install -g crypto-js
+cd /ql/data/scripts && pnpm install -g crypto -g
+cd /ql/data/scripts && pnpm install -g download
+cd /ql/data/scripts && pnpm install -g typescript
+cd /ql/data/scripts && pnpm install -g png-js
+
+cd /ql/data/scripts && pnpm install -g got@11
+cd /ql/data/scripts && pnpm install -g ts-node
+cd /ql/data/scripts && pnpm install -g @types/node
+cd /ql/data/scripts && pnpm install -g canvas
+
+
+
+
+
+
+
+
