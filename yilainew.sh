1ec40d25b74c9cfbe2b2286d1eaf4690d5e87541
Jumeng-ms <465243525@qq.com>
2022-05-28 23:25:35 +0800

    Update yilainew.sh
    更新新版依赖

--- yilainew.sh
+++ yilainew.sh
@@ -40,3 +40,46 @@ cd /ql/data/scripts && pnpm install -g canvas
 
 
 
+cd /ql/data/scripts && pip3 install requests
+cd /ql/data/scripts && pip3 install --upgrade pip
+cd /ql/data/scripts && pip3 install jieba
+cd /ql/data/scripts && pip3 install telethon
+cd /ql/data/scripts && pip3 install qrcode
+cd /ql/data/scripts && pip3 install PyExecJS
+cd /ql/data/scripts && pip3 install aiohttp
+cd /ql/data/scripts && pip3 install PySocks
+
+cd /ql/data/scripts && apk add libc6-compat
+cd /ql/data/scripts && apk add nodejs-current
+cd /ql/data/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev    
+cd /ql/data/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
+
+
+cd /ql/data/scripts && npm install -g pnpm
+cd /ql/data/scripts && npm install -g @otplib/preset-default
+cd /ql/data/scripts && npm install -g js-base64
+cd /ql/data/scripts && npm install -g fund
+cd /ql/data/scripts && npm install -g jsdom
+cd /ql/data/scripts && npm install -g form-data
+cd /ql/data/scripts && npm install -g tough-cookie
+cd /ql/data/scripts && npm install -g axios date-fns
+cd /ql/data/scripts && npm install -g crypto-js
+cd /ql/data/scripts && npm install -g crypto -g
+cd /ql/data/scripts && npm install -g download
+cd /ql/data/scripts && npm install -g typescript
+cd /ql/data/scripts && npm install -g png-js
+
+cd /ql/data/scripts && npm install -g got@11
+cd /ql/data/scripts && npm install -g ts-node
+cd /ql/data/scripts && npm install -g @types/node
+cd /ql/data/scripts && npm install -g canvas
+cd /ql/data/scripts && npm install -g ts-md5
+cd /ql/data/scripts && npm install -g ws
+cd /ql/data/scripts && npm install -g request
+
+
+
+
+
+
+
