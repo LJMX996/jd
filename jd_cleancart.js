7d040fea8fd43c8ced2eb693599662e3aa8e2ac0
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-06-04 17:19:27 +0800

    jd_cleancart.js

--- jd_cleancart.js
+++ jd_cleancart.js
@@ -27,7 +27,7 @@ let args_xh = {
      * 控制脚本运行一次取消多少条购物车数据，为0则不删除，默认为100
      * 环境变量名称：XH_CLEAN_REMOVESIZE
      */
-    removeSize: process.env.XH_CLEAN_REMOVESIZE * 1 || 200,
+    removeSize: process.env.XH_CLEAN_REMOVESIZE * 1 || 100,
     /*
      * 关键字/关键词设置，当购物车商品标题包含关键字/关键词时，跳过该条数据，多个则用@分隔，例如 旺仔牛奶@红外线@紫光
      * 环境变量名称：XH_CLEAN_KEYWORDS
@@ -85,7 +85,9 @@ if ($.isNode()) {
                     $.keywordsNum = 0
                     if ($.beforeRemove !== "0") {
                         await cartFilter_xh(venderCart);
+												$.retry = 0;
                         if (parseInt($.beforeRemove) !== $.keywordsNum) await removeCart();
+												if($.retry = 2) break;
                         else {
                             console.log('\n由于购物车内的商品均包含关键字，本次执行将不删除购物车数据')
                             break;
@@ -115,13 +117,12 @@ function getCart_xh() {
         }
         $.get(option, async (err, resp, data) => {
             try {
-                let content = getSubstr(data, "window.cartData = ", "window._PFM_TIMING").replace(/\s*/g, "");
-                data = JSON.parse(content);
+                data = JSON.parse(data.match(/window\.cartData = ([^;]*)/)[1])
                 $.areaId = data.areaId;   // locationId的传值
                 $.traceId = data.traceId; // traceid的传值
                 venderCart = data.cart.venderCart;
                 postBody = 'pingouchannel=0&commlist=';
-                $.beforeRemove = data.cartJson.num
+                $.beforeRemove = data.cart.currentCount ? data.cart.currentCount : 0;
                 console.log(`获取到购物车数据 ${$.beforeRemove} 条`)
             } catch (e) {
                 $.logErr(e, resp);
@@ -195,6 +196,7 @@ function removeCart() {
                     console.log('删除失败')
                     console.log(data.errMsg)
                     $.error = true;
+										$.retry++;
                 }
             } catch (e) {
                 $.logErr(e, resp);
@@ -203,13 +205,6 @@ function removeCart() {
             }
         });
     })
-}
-
-function getSubstr(str, leftStr, rightStr) {
-    let left = str.indexOf(leftStr);
-    let right = str.indexOf(rightStr, left);
-    if (left < 0 || right < left) return '';
-    return str.substring(left + leftStr.length, right);
 }
 
 function TotalBean() {
