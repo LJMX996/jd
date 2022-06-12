0842eee8147329301b3ccf2fe34bd646df588327
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-06-13 01:49:59 +0800

    yilai for mianban.sh

---
+++ yilai for mianban.sh
@@ -0,0 +1,58 @@
+## nodejs
+
+@otplib/preset-default
+js-base64
+fund
+jsdom
+form-data
+tough-cookie
+axios
+date-fns
+crypto-js
+crypto
+download
+typescript
+png-js
+got@11
+ts-node
+@types/node
+canvas
+ts-md5
+ws
+request
+tslib
+requests
+tunnel
+form-data
+fs
+tencentcloud-sdk-nodejs
+
+
+## python
+requests 
+--upgrade pip 
+jieba 
+telethon 
+qrcode 
+PyExecJS 
+aiohttp 
+PySocks 
+beautifulsoup4
+
+
+## linux
+libc6-compat
+nodejs-current
+python3 zlib-dev
+gcc
+jpeg-dev
+python3-dev
+musl-dev
+freetype-dev
+--no-cache
+build-base
+g++
+cairo-dev
+pango-dev 
+giflib-dev
+build-base
