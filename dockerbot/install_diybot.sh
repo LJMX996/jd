9e7c7c64f5036fe3640da1ff9f66896fbf703fb7
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-07-19 11:18:47 +0800

    bean_Global.py

--- dockerbot/install_diybot.sh
+++ dockerbot/install_diybot.sh
@@ -2,6 +2,10 @@ cd /ql/repo && git clone -b aaron https://github.com/LJMX996/jd.git
 
 cd /ql/repo/jd && git pull
 
+cd /ql/repo && git clone -b main https://github.com/ccwav/QLScript2.git ccwav_QLScript2
+
+cd /ql/repo/ccwav_QLScript2 && git pull
+
 cp -rfv /ql/repo/jd/dockerbot/ /ql/repo/
 
 cp -rfv /ql/repo/jd/dockerbot/jbot/bot /ql/jbot/
