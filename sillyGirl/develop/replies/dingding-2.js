01961f72ac6ff590d935cbe0091f237671175051
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-08-29 19:22:56 +0800

    dingding-2.js

---
+++ sillyGirl/develop/replies/dingding-2.js
@@ -0,0 +1,18 @@
+
+// [cron: 0,5,10 18 * * *]
+var ele = "https://i.loli.net/2021/12/03/MS4LKzTyd5WCc1X.jpg";
+//图文消息
+var content = "\n愉快的一天从下班开始，\n 记得钉钉打卡哦！！！";
+var imType = ImType();
+if (imType == "fake") {
+    var groups = [{
+        imType: "wx",
+        groupCode: 48102851005,
+    }]
+    for (var i = 0; i < groups.length; i++) {
+        groups[i]["content"] = content
+        push(groups[i])
+    }
+} else {
+    sendImage(ele)
+}
---
+++ sillyGirl/develop/replies/dingding.js
@@ -0,0 +1,18 @@
+
+// [cron: 0,5,10 8 * * *]
+var ele = "https://i.loli.net/2021/12/03/MS4LKzTyd5WCc1X.jpg";
+//图文消息
+var content = "\n上班了，钉钉打卡咯！！！";
+var imType = ImType();
+if (imType == "fake") {
+    var groups = [{
+        imType: "wx",
+        groupCode: 48102851005,
+    }]
+    for (var i = 0; i < groups.length; i++) {
+        groups[i]["content"] = content
+        push(groups[i])
+    }
+} else {
+    sendImage(ele)
+}
