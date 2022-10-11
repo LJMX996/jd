360f84f712284b249d49f882e10c4dd03bc697d5
LJMX996 <52339994+LJMX996@users.noreply.github.com>
2022-10-12 07:09:02 +0800

    dingding.js

--- sillyGirl/develop/replies/dingding.js
+++ sillyGirl/develop/replies/dingding.js
@@ -1,8 +1,8 @@
 
-// [cron: 0,5,10,18 8 * * 1-5]
+// [cron: 0,5,10,20 8 * * 1-5]
 var ele = "https://i.loli.net/2021/12/03/MS4LKzTyd5WCc1X.jpg";
 //图文消息
-var content = "\n上班了，钉钉打卡咯！！！";
+var content = "\n上班摸鱼了，\n 记得钉钉打卡撒！！！";
 var imType = ImType();
 if (imType == "fake") {
     var groups = [{
